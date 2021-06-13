a1b4ec18721dfa8a255f5b6bf8fe38eae6707a14
shankrv <ravis.webd@gmail.com>
2021-06-13 12:32:16 +0530

    state, useState() and events

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,14 +1,24 @@
+import React, { useState } from 'react';
+
 import Card from '../Interface/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title); // useState() returns [value, updateFx]
+
+  const clickHandler = () => {
+    setTitle('Updated'); // updates and re-renders the component
+    console.log('clicked', title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h3> {props.title} </h3>
+        <h3> {title} </h3>
         <div className="expense-item__price"> ₹ {props.price} </div>
+        <button onClick={clickHandler}>Click</button>
       </div>
     </Card>
   );
